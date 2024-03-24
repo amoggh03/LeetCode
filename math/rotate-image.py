5da5a8e6180a40913689c6c39404b4f95feae589
amoggh03 <104645849+amoggh03@users.noreply.github.com>
2024-03-24 19:39:42 +0530

    :ballot_box_with_check: rotate-image [Runtime: 42 ms; Memory: 16.5 MB]

---
+++ math/rotate-image.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        matrix[:] = zip(*matrix[::-1])
+        return matrix
